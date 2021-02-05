2dafb3ba5b616aaea06a07d7be2b157c2f380279
stepanturchenko <stepanstepan4@gmail.com>
2021-02-05 17:53:45 +0200

    add 3 models for testing and check light types

--- public/index.js
+++ public/index.js
@@ -3,12 +3,24 @@ import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources
 import { OBJLoader } from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/loaders/MTLLoader.js';
 import { initial } from '/initial.js'
-
+import { GUI } from 'https://threejsfundamentals.org/threejs/../3rdparty/dat.gui.module.js';
+const dat = new GUI()
+const max_gui = 10
 initial()
+
+const addXYZGUI = (prop, name) => {
+    const folder = dat.addFolder(name)
+    folder.add(prop, 'x', -max_gui, max_gui)
+    folder.add(prop, 'y', -max_gui, max_gui)
+    folder.add(prop, 'z', -max_gui, max_gui)
+    folder.open()
+}
 
 function main() {
     const canvas = document.querySelector('canvas');
     const renderer = new THREE.WebGLRenderer({ canvas });
+    //SHADOW
+    renderer.shadowMap.enabled = true
 
     const fov = 45;
     const aspect = 2;  // the canvas default
@@ -41,6 +53,10 @@ function main() {
             side: THREE.DoubleSide,
         });
         const mesh = new THREE.Mesh(planeGeo, planeMat);
+        //SHADOW
+        mesh.receiveShadow = true;
+        mesh.castShadow = true;
+
         mesh.rotation.x = Math.PI * -.5;
         scene.add(mesh);
     }
@@ -54,34 +70,58 @@ function main() {
     }
 
     {
-        const color = 0xFFFFFF;
-        const intensity = 1;
-        const light = new THREE.DirectionalLight(color, intensity);
-        light.position.set(5, 10, 2);
-        scene.add(light);
-        scene.add(light.target);
+        // const color = 0xFFFFFF;
+        // const intensity = 1;
+        // const light = new THREE.DirectionalLight(color, intensity);
+        // light.position.set(5, 10, 2);
+        // light.castShadow = true
+        // addXYZGUI(light.position, 'light')
+        // scene.add(light);
+        // scene.add(light.target);
+
+        // const cameraHelper = new THREE.CameraHelper(light.shadow.camera);
+        // scene.add(cameraHelper);
     }
 
     {
-        // const mtlLoader = new MTLLoader();
-        // mtlLoader.load('https://threejsfundamentals.org/threejs/resources/models/windmill/windmill.mtl', (mtl) => {
-        //     mtl.preload();
-        //     const objLoader = new OBJLoader();
-        //     objLoader.setMaterials(mtl);
-        //     objLoader.load('https://threejsfundamentals.org/threejs/resources/models/windmill/windmill.obj', (root) => {
-        //         scene.add(root);
-        //     });
-        // });
-
         const mtlLoader = new MTLLoader();
+
+        mtlLoader.load('https://threejsfundamentals.org/threejs/resources/models/windmill/windmill.mtl', (mtl) => {
+            mtl.preload();
+            const objLoader = new OBJLoader();
+            objLoader.setMaterials(mtl);
+            objLoader.load('https://threejsfundamentals.org/threejs/resources/models/windmill/windmill.obj', (root) => {
+                root.position.x -= 5
+                root.castShadow = true;
+                root.receiveShadow = true;
+                scene.add(root);
+            });
+        });
+
         mtlLoader.load('./models/meat_boy_obj/Super_meatboy_free.mtl', (mtl) => {
             mtl.preload();
             const objLoader = new OBJLoader();
             objLoader.setMaterials(mtl);
             objLoader.load('./models/meat_boy_obj/Super_meatboy_free.obj', (root) => {
+                root.position.x += 5
+                root.castShadow = true;
+                root.receiveShadow = true;
                 scene.add(root);
             });
         });
+
+        const sphereRadius = 2;
+        const sphereWidthDivisions = 32;
+        const sphereHeightDivisions = 16;
+        const sphereGeo = new THREE.SphereBufferGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
+        const sphereMat = new THREE.MeshPhongMaterial({ color: '#CA8' });
+        const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+        mesh.castShadow = true;
+        mesh.receiveShadow = true;
+        mesh.position.y = 3
+        addXYZGUI(mesh.position, 'sphere')
+        scene.add(mesh);
+
 
     }
 
